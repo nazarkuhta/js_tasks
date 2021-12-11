f7c7089eca5c625f1a91108eb0bd34d3b59e20d5
nazaekuhta <nazar.kuhta.2004@gmail.com>
2021-12-11 13:35:43 +0200

    .

--- practic12/pr.js
+++ practic12/pr.js
@@ -2,3 +2,14 @@
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+
+
+fetch(`https://jsonplaceholder.typicode.com/posts`).then(response => response.json()).then(posts => {
+
+
+    for (const post of posts) {
+
+    }
+
+
+})
